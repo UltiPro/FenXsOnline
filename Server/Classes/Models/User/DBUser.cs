7cec14b33ee29eb658afdef7f448c291a67e2dab
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-10-24 00:28:53 +0200

    Add User DB Model

---
+++ Server/Classes/Models/User/DBUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Classes.Models.Hero;
+
+namespace Classes.Models.User;
+
+public class DBUser : IdentityUser
+{
+    public bool IsBanned { get; set; }
+    public virtual IList<DBHero> Heros { get; set; }
+}
