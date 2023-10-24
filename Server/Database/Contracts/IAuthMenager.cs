9e013fbfe2cf33fc06a2d1a7fc84b624be120f5a
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-10-24 23:33:48 +0200

    Add Auth Contract Interface

---
+++ Server/Database/Contracts/IAuthMenager.cs
@@ -0,0 +1,13 @@
+﻿using Classes.Models.Auth;
+using Classes.Models.User;
+using Microsoft.AspNetCore.Identity;
+
+namespace Database.Contracts;
+
+public interface IAuthMenager
+{
+    Task<IEnumerable<IdentityError>> Register(UserRegister userRegister);
+    Task<AuthResponse> Login(UserLogin userLogin);
+    Task<string> RefreshToken();
+    Task<AuthResponse> VerifyRefreshToken(AuthResponse authResponse);
+}
