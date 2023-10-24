dd732176401d9011e12baeb2f68ad84815e2a2ae
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-10-24 23:33:02 +0200

    Add User Auth Models

---
+++ Server/Classes/Models/User/UserRegister.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Classes.Models.User;
+
+public class UserRegister : UserLogin
+{
+    [Required]
+    [EmailAddress(ErrorMessage = "Incorrect e-mail address.")]
+    public string Email { get; set; }
+}
