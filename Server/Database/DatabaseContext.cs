1924709704e20bf5aad7f7abd3f5a00f50e9bbfb
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-10-27 18:55:15 +0200

    Add Basic Database Context with roles added

---
+++ Server/Database/DatabaseContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Classes.Models.User;
+using Database.Configuration;
+
+namespace Database;
+
+public class DatabaseContext : IdentityDbContext<DBUser>
+{
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+    public DbSet<DBUser> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfiguration(new RoleConfiguration());
+    }
+}
