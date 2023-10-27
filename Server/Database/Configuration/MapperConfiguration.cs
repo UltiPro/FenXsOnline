0387bed1422b32d1a0b54f1473f2e8d6ab8866e6
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-10-27 18:54:50 +0200

    Add Basic Mapper

---
+++ Server/Database/Configuration/MapperConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using Classes.Models.User;
+
+namespace Database.Configuration;
+
+public class MapperConfiguration : Profile
+{
+    public MapperConfiguration()
+    {
+        CreateMap<UserRegister, DBUser>().ReverseMap();
+    }
+}
