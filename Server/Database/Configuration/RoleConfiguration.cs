732a791437ec553f4b1f62f59b898ad5e1e69c0d
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-10-27 18:53:04 +0200

    Add roles configuration class

---
+++ Server/Database/Configuration/RoleConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Database.Configuration;
+
+public class RoleConfiguration : IEntityTypeConfiguration<IdentityRole>
+{
+    public void Configure(EntityTypeBuilder<IdentityRole> entityTypeBuilder)
+    {
+        entityTypeBuilder.HasData(
+            new IdentityRole
+            {
+                Name = "User",
+                NormalizedName = "USER"
+            },
+            new IdentityRole
+            {
+                Name = "Moderator",
+                NormalizedName = "MOD"
+            },
+            new IdentityRole
+            {
+                Name = "Administrator",
+                NormalizedName = "ADMIN"
+            }
+            );
+    }
+}
