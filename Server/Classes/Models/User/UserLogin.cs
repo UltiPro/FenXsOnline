bd21aa85770ed48b0d4bd548850ee9ba71cbe622
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-10-28 21:15:59 +0200

    Removed one validation condition (useless)

--- Server/Classes/Models/User/UserLogin.cs
+++ Server/Classes/Models/User/UserLogin.cs
@@ -5,11 +5,9 @@ namespace Classes.Models.User;
 public class UserLogin
 {
     [Required(ErrorMessage = "Login is required.")]
-    [StringLength(30, ErrorMessage = "The login must be between 4 and 30 characters long and must start with a letter and end with a letter or number. May contain a floor and a dash between the start and end.", MinimumLength = 4)]
     [RegularExpression(@"^(?=.*?[a-zA-Z\d])[a-zA-Z][a-zA-Z\d_-]{2,28}[a-zA-Z\d]$", ErrorMessage = "The login must be between 4 and 30 characters long and must start with a letter and end with a letter or number. May contain a floor and a dash between the start and end.")]
     public string Login { get; set; }
     [Required(ErrorMessage = "Password is required.")]
-    [StringLength(40, ErrorMessage = "Password must be between 8 and 40 characters long and contain at least one lowercase letter, one uppercase letter, one number and one special character.", MinimumLength = 8)]
     [RegularExpression(@"^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[.~!@#$%^&*()+=[\]\\;:'""/,|{}<>?])\S{8,40}$", ErrorMessage = "Password must be between 8 and 40 characters long and contain at least one lowercase letter, one uppercase letter, one number and one special character.")]
     public string Password { get; set; }
 }
