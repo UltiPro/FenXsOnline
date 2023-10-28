31db057668df6035e5e241e0602deafbfeb4e866
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-10-28 21:17:39 +0200

    Added Basic DBHero model

---
+++ Server/Classes/Models/Hero/DBHero.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Classes.Models.Hero;
+
+public class DBHero
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
