ad9ca5ff52cb818680c9331340bbb6a1757f8146
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-10-29 21:59:36 +0100

    Delete DBHero.cs

--- Server/Classes/Models/Hero/DBHero.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-
-namespace Classes.Models.Hero;
-
-public class DBHero
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-}
