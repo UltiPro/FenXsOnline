83adc6fb374591b8cd7ad1547f92310f41e81e25
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-10-29 23:19:54 +0100

    Added Interface for Admin Menager

---
+++ Server/Database/Contracts/IAdminMenager.cs
@@ -0,0 +1,14 @@
+﻿using Classes.Models.Admin;
+
+namespace Database.Contracts;
+
+public interface IAdminMenager
+{
+    Task<bool> SetAdminByCode(AdminSetByCode adminSetByCode);
+    Task<bool> SetAdmin(string id);
+    Task<bool> UnsetAdmin(string id);
+    Task<bool> SetModerator(string id);
+    Task<bool> UnsetModerator(string id);
+    Task<bool> BanUser(string id);
+    Task<bool> UnbanUser(string id);
+}
