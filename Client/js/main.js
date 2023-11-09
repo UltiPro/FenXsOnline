82f1884883f7b082ae9322e3ca85a8fe31029ea3
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-11-09 20:21:36 +0100

    Update main.js

--- Client/js/main.js
+++ Client/js/main.js
@@ -2,10 +2,10 @@ const changeInputAudio = new Audio("./audio/input.wav");
 const changeWindowAudio = new Audio("./audio/window.wav");
 const unsuccessAudio = new Audio("./audio/unsuccess.wav");
 
-const playChangeInputAudio = () => changeInputAudio.play();
-const playChangeWindowAudio = () => changeWindowAudio.play();
-const playUnsuccessAudio = () => unsuccessAudio.play();
+const PlayChangeInputAudio = () => changeInputAudio.play();
+const PlayChangeWindowAudio = () => changeWindowAudio.play();
+const PlayUnsuccessAudio = () => unsuccessAudio.play();
 
 $(document).ready(function () {
-    $("input").bind("focus", playChangeInputAudio);
+    $("input").bind("focus", PlayChangeInputAudio);
 });
