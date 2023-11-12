7c1524660ea5696f369d5c28b822b494d0ef8485
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-11-12 19:01:20 +0100

    Added Base Item Model

--- Server/Classes/Models/Game/Item/BaseItem.cs
+++ Server/Classes/Models/Game/Item/BaseItem.cs
@@ -3,12 +3,13 @@
 public class BaseItem
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public int Level { get; set; }
-    public int HealthPoints { get; set; }
-    public int Stamina { get; set; }
-    public int Mana { get; set; }
-    public int Weight { get; set; }
-    public int Price { get; set; }
-    public bool AvailableInFight { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public int Level { get; set; } = 1;
+    public int HealthPoints { get; set; } = 0;
+    public int Agility { get; set; } = 0;
+    public int Stamina { get; set; } = 0;
+    public int Mana { get; set; } = 0;
+    public int Weight { get; set; } = 1;
+    public int Price { get; set; } = 1;
+    public bool AvailableInFight { get; set; } = false;
 }
