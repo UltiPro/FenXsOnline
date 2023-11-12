ba95843b377bacb9f9843dd7cc3fea7b2077750f
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-11-12 19:47:16 +0100

    Update BaseItem.cs

--- Server/Classes/Models/Game/Item/BaseItem.cs
+++ Server/Classes/Models/Game/Item/BaseItem.cs
@@ -3,13 +3,9 @@
 public class BaseItem
 {
     public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
+    public string Name { get; set; }
     public int Level { get; set; } = 1;
-    public int HealthPoints { get; set; } = 0;
-    public int Agility { get; set; } = 0;
-    public int Stamina { get; set; } = 0;
-    public int Mana { get; set; } = 0;
     public int Weight { get; set; } = 1;
     public int Price { get; set; } = 1;
-    public bool AvailableInFight { get; set; } = false;
+    public bool QuestItem { get; set; } = false;
 }
