fb188e6f5efe7174a14a317243b01a83fe8e6dd7
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-11-12 23:30:10 +0100

    Added static roles

---
+++ Server/Classes/Statics/Roles.cs
@@ -0,0 +1,11 @@
+﻿namespace Classes.Statics;
+
+public static class Roles
+{
+    public static readonly string User = "User";
+    public static readonly string UserN = "USER";
+    public static readonly string Mod = "Moderator";
+    public static readonly string ModN = "MOD";
+    public static readonly string Admin = "Administrator";
+    public static readonly string AdminN = "ADMIN";
+}
