e7dc81bd91b8626d7e3b69d173b632ff33dc8e74
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-11-12 20:14:27 +0100

    Added base item armor model

---
+++ Server/Classes/Models/Game/Item/BaseItemArmor.cs
@@ -0,0 +1,9 @@
+﻿namespace Classes.Models.Game.Item;
+
+public class BaseItemArmor : BaseItem
+{
+    public int Armor { get; set; } = 1;
+    public int MagicArmor { get; set; } = 0;
+    public int Agility { get; set; } = 0;
+    public int HealthPoints { get; set; } = 0;
+}
