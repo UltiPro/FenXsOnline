d2f01499ab6826785cc17791afc2d640613def70
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-11-12 14:13:30 +0100

    Update DBUser.cs

--- Server/Classes/Models/User/DBUser.cs
+++ Server/Classes/Models/User/DBUser.cs
@@ -1,10 +1,9 @@
-﻿using Classes.Models.Game.Hero;
 using Microsoft.AspNetCore.Identity;
+using Classes.Models.Game.Hero;
 
 namespace Classes.Models.User;
 
 public class DBUser : IdentityUser
 {
-    public bool IsBanned { get; set; }
     public virtual IList<DBHero> Heroes { get; set; }
 }
