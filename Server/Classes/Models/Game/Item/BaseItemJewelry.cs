42a0d4561b2c1f6e5ef8b2bf20dda6075b7116a7
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-11-12 20:03:22 +0100

    Added base jewelry item model

---
+++ Server/Classes/Models/Game/Item/BaseItemJewelry.cs
@@ -0,0 +1,9 @@
+﻿namespace Classes.Models.Game.Item;
+
+public class BaseItemJewelry : BaseItem
+{
+    public int HealthPoints { get; set; } = 0;
+    public int Stamina { get; set; } = 0;
+    public int Mana { get; set; } = 0;
+    public int CriticalChance { get; set; } = 0;
+}
