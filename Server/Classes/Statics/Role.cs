cebd7f651fd79bc4ba5f8e441e45553b24256a9a
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-11-14 19:38:58 +0100

    Update Role.cs

--- Server/Classes/Statics/Role.cs
+++ Server/Classes/Statics/Role.cs
@@ -5,7 +5,7 @@ public static class Role
     public static readonly string User = "User";
     public static readonly string UserN = "USER";
     public static readonly string Mod = "Moderator";
-    public static readonly string ModN = "MOD";
+    public static readonly string ModN = "MODERATOR";
     public static readonly string Admin = "Administrator";
-    public static readonly string AdminN = "ADMIN";
+    public static readonly string AdminN = "ADMINISTRATOR";
 }
