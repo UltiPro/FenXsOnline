f001a2c392e714d8a932a74ffc56627877f27b76
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-11-14 19:38:49 +0100

    Update IAdminMenager.cs

--- Server/Database/Contracts/IAdminMenager.cs
+++ Server/Database/Contracts/IAdminMenager.cs
@@ -4,11 +4,11 @@ namespace Database.Contracts;
 
 public interface IAdminMenager
 {
-    Task<bool> SetAdminByCode(AdminSetByCode adminSetByCode);
-    Task<bool> SetAdmin(string id);
-    Task<bool> UnsetAdmin(string id);
-    Task<bool> SetModerator(string id);
-    Task<bool> UnsetModerator(string id);
-    Task<bool> BanUser(string id);
-    Task<bool> UnbanUser(string id);
+    Task SetAdminByCode(AdminSetByCode adminSetByCode);
+    Task SetAdmin(string id);
+    Task UnsetAdmin(string id);
+    Task SetModerator(string id);
+    Task UnsetModerator(string id);
+    Task BanUser(string id);
+    Task UnbanUser(string id);
 }
