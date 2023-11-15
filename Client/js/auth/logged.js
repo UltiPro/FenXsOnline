1485a559c43011eb8f586f435643c24c38178311
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-11-15 17:55:55 +0100

    Added auth js files

---
+++ Client/js/auth/logged.js
@@ -0,0 +1,1 @@
+app.post(apiBaseUrl + "Account/refreshtoken").then(_ => window.location.replace("./start.html"), _ => _);
---
+++ Client/js/auth/unlogged.js
@@ -0,0 +1,1 @@
+app.post(apiBaseUrl + "Account/refreshtoken").then(_ => _, _ => window.location.replace("./index.html"));
