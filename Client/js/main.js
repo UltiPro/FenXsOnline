4e7951dd60ceb21814732a9e1f74415748bd1987
Dawid Majewski <80513@student.pb.edu.pl>
2023-11-16 20:05:42 +0100

    main.js fix
    Optimized function for playing audio

--- Client/js/main.js
+++ Client/js/main.js
@@ -15,12 +15,6 @@ const PlayUnsuccessAudio = () => unsuccessAudio.play();
 
 $(document).ready(function () {
     $("input").bind("focus", PlayChangeInputAudio);
-});
-
-$(document).ready(function () {
+    $("select").bind("focus", PlayChangeInputAudio);
     $("select").bind("change", PlayChangeInputAudio);
 });
-$(document).ready(function () {
-    $("select").bind("focus", PlayChangeInputAudio);
-});
-
--- Client/js/start.js
+++ Client/js/start.js
@@ -37,7 +37,7 @@ $(document).ready(function () {
                 return "No description available.";
         }
     }
+
+
+    
 });
-
-
-
