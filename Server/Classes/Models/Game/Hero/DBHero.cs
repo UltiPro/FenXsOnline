1df8cebcbf86632a39bc373438e2c89ab74785d4
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-11-17 01:22:53 +0100

    Added and updated Hero models

--- Server/Classes/Models/Game/Hero/DBHero.cs
+++ Server/Classes/Models/Game/Hero/DBHero.cs
@@ -1,9 +1,33 @@
-﻿namespace Classes.Models.Game.Hero;
+﻿using Classes.Models.Game.Item.Armor;
+using Classes.Models.Game.Item.Boots;
+using Classes.Models.Game.Item.Gloves;
+using Classes.Models.Game.Item.Helmet;
+using Classes.Models.Game.Item.Necklace;
+using Classes.Models.Game.Item.Ring;
+using Classes.Models.Game.Item.SecondaryWeapon;
+using Classes.Models.Game.Item.Weapon;
 
-public class DBHero
+namespace Classes.Models.Game.Hero;
+
+public class DBHero : HeroBasicInfo
 {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public int HealthPoints { get; set; }
-    public int MaxHealthPoints { get; set; }
+    public int Atack { get; set; } = 0;
+    public int MagicAtack { get; set; } = 0;
+    public int CriticalChance { get; set; } = 0;
+    public int Armor { get; set; } = 1;
+    public int MagicArmor { get; set; } = 0;
+    public int Agility { get; set; } = 0;
+    public int Weight { get; set; } = 1;
+    public int Gold { get; set; } = 0;
+    public int MapId { get; set; } = 0;
+    public int X { get; set; } = 3;
+    public int Y { get; set; } = 2;
+    public DBArmor? armor { get; set; } = null;
+    public DBBoots? boots { get; set; } = null;
+    public DBGloves? gloves { get; set; } = null;
+    public DBHelmet? helmet { get; set; } = null;
+    public DBNecklace? necklace { get; set; } = null;
+    public DBRing? ring { get; set; } = null;
+    public DBSecondaryWeapon? secondaryWeapon { get; set; } = null;
+    public DBWeapon? weapon { get; set; } = null;
 }
---
+++ Server/Classes/Models/Game/Hero/HeroBasicInfo.cs
@@ -0,0 +1,19 @@
+﻿using Classes.Enums.Game;
+
+namespace Classes.Models.Game.Hero;
+
+public class HeroBasicInfo
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Level { get; set; } = 1;
+    public Profession Profession { get; set; }
+    public Sex Sex { get; set; }
+    public int HealthPoints { get; set; } = 100;
+    public int MaxHealthPoints { get; set; } = 100;
+    public int Mana { get; set; } = 0;
+    public int MaxMana { get; set; } = 0;
+    public int Stamina { get; set; } = 0;
+    public int MaxStamina { get; set; } = 0;
+    public string Sprite { get; set; }
+}
