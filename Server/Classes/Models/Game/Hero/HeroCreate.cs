7e187e5cea87fbab9d14c45a8d8996fca80ae77f
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-11-17 22:38:02 +0100

    Added validation model and inheritance

--- Server/Classes/Models/Game/Hero/HeroCreate.cs
+++ Server/Classes/Models/Game/Hero/HeroCreate.cs
@@ -3,11 +3,8 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Classes.Models.Game.Hero;
 
-public class HeroCreate
+public class HeroCreate : HeroName
 {
-    [Required(ErrorMessage = "Hero name is required.")]
-    [RegularExpression(@"^[a-zA-Z]\w{2,15}$", ErrorMessage = "A nickname cannot be shorter than 3 characters and longer than 15 characters.")]
-    public string Name { get; set; }
     [Required(ErrorMessage = "Hero profession is required.")]
     [Range(0, 3)]
     public Profession Profession { get; set; }
---
+++ Server/Classes/Models/Game/Hero/HeroName.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Classes.Models.Game.Hero;
+
+public class HeroName
+{
+    [Required(ErrorMessage = "Hero name is required.")]
+    [RegularExpression(@"^[a-zA-Z]\w{2,15}$", ErrorMessage = "A nickname cannot be shorter than 3 characters and longer than 15 characters.")]
+    public string Name { get; set; }
+}
