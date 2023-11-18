d38703bfd1b24085c7216e02b90415095fcef168
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-11-18 01:09:25 +0100

    Update HeroCreate.cs

--- Server/Classes/Models/Game/Hero/HeroCreate.cs
+++ Server/Classes/Models/Game/Hero/HeroCreate.cs
@@ -3,8 +3,11 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Classes.Models.Game.Hero;
 
-public class HeroCreate : HeroName
+public class HeroCreate
 {
+    [Required(ErrorMessage = "Hero name is required.")]
+    [RegularExpression(@"^[a-zA-Z]\w{2,15}$", ErrorMessage = "A nickname cannot be shorter than 3 characters and longer than 15 characters.")]
+    public string Name { get; set; }
     [Required(ErrorMessage = "Hero profession is required.")]
     [Range(0, 3)]
     public Profession Profession { get; set; }
