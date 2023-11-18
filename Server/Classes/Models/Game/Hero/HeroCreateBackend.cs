a7a9b945b363a5b1fce5b3cf902300e100752f5a
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-11-18 18:23:37 +0100

    Correct

--- Server/Classes/Models/Game/Hero/HeroCreateBackend.cs
+++ Server/Classes/Models/Game/Hero/HeroCreateBackend.cs
@@ -12,28 +12,23 @@ public class HeroCreateBackend : HeroCreate
         Profession = heroCreate.Profession;
         Sex = heroCreate.Sex;
         UserId = userId;
-        switch (Sex)
+        switch (Profession)
         {
-            case Sex.Male:
-                switch (Profession)
-                {
-                    case Profession.Warrior:
-                        if (Sex == Sex.Male) SpriteURL = "warriorM0.gif";
-                        else SpriteURL = "warriorF0.gif";
-                        break;
-                    case Profession.Hunter:
-                        if (Sex == Sex.Male) SpriteURL = "hunterM0.gif";
-                        else SpriteURL = "hunterF0.gif";
-                        break;
-                    case Profession.Mage:
-                        if (Sex == Sex.Male) SpriteURL = "mageM0.gif";
-                        else SpriteURL = "mageF0.gif";
-                        break;
-                    case Profession.Paladin:
-                        if (Sex == Sex.Male) SpriteURL = "paladinM0.gif";
-                        else SpriteURL = "paladinF0.gif";
-                        break;
-                }
+            case Profession.Warrior:
+                if (Sex == Sex.Male) SpriteURL = "warriorM0.gif";
+                else SpriteURL = "warriorF0.gif";
+                break;
+            case Profession.Hunter:
+                if (Sex == Sex.Male) SpriteURL = "hunterM0.gif";
+                else SpriteURL = "hunterF0.gif";
+                break;
+            case Profession.Mage:
+                if (Sex == Sex.Male) SpriteURL = "mageM0.gif";
+                else SpriteURL = "mageF0.gif";
+                break;
+            case Profession.Paladin:
+                if (Sex == Sex.Male) SpriteURL = "paladinM0.gif";
+                else SpriteURL = "paladinF0.gif";
                 break;
         }
     }
