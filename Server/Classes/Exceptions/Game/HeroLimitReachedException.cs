67a2a9337c2f07007d75e2a1fa9f7f0ec8a88555
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-11-18 18:23:28 +0100

    Added Heroes limit exception

---
+++ Server/Classes/Exceptions/Game/HeroLimitReachedException.cs
@@ -0,0 +1,6 @@
+﻿namespace Classes.Exceptions.Game;
+
+public class HeroLimitReachedException : Exception
+{
+    public HeroLimitReachedException() : base("Your account already has the maximum number of heroes.") { }
+}
