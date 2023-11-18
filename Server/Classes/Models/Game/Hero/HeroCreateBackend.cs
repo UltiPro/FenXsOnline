7f57831408b86ee5af522f6638954c60b82a9e9d
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-11-18 01:09:48 +0100

    Added backend model for create hero

---
+++ Server/Classes/Models/Game/Hero/HeroCreateBackend.cs
@@ -0,0 +1,40 @@
+﻿using Classes.Enums.Game;
+
+namespace Classes.Models.Game.Hero;
+
+public class HeroCreateBackend : HeroCreate
+{
+    public string UserId { get; set; }
+    public string SpriteURL { get; set; }
+    public HeroCreateBackend(HeroCreate heroCreate, string userId)
+    {
+        Name = heroCreate.Name;
+        Profession = heroCreate.Profession;
+        Sex = heroCreate.Sex;
+        UserId = userId;
+        switch (Sex)
+        {
+            case Sex.Male:
+                switch (Profession)
+                {
+                    case Profession.Warrior:
+                        if (Sex == Sex.Male) SpriteURL = "warriorM0.gif";
+                        else SpriteURL = "warriorF0.gif";
+                        break;
+                    case Profession.Hunter:
+                        if (Sex == Sex.Male) SpriteURL = "hunterM0.gif";
+                        else SpriteURL = "hunterF0.gif";
+                        break;
+                    case Profession.Mage:
+                        if (Sex == Sex.Male) SpriteURL = "mageM0.gif";
+                        else SpriteURL = "mageF0.gif";
+                        break;
+                    case Profession.Paladin:
+                        if (Sex == Sex.Male) SpriteURL = "paladinM0.gif";
+                        else SpriteURL = "paladinF0.gif";
+                        break;
+                }
+                break;
+        }
+    }
+}
