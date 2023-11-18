cbbe10148c03659c5e6c76a75609b7e0f7f85cf7
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-11-19 00:47:31 +0100

    Added Hero Backend logic

---
+++ Server/Database/Contracts/IHeroMenager.cs
@@ -0,0 +1,14 @@
+﻿using Classes.Models.Game.Hero;
+
+namespace Database.Contracts;
+
+public interface IHeroMenager : IGenericRepository<DBHero>
+{
+    public Task<bool> IsAvailableNickname(string nickname);
+    public Task<bool> IsHeroLimitReached(string accountId);
+    public Task<bool> IsHeroThisUser(string accountId, int heroId);
+    public Task<List<HeroBasicInfo>> GetHeroes(string accountId);
+    public Task ChangeSprite(int heroId, int spriteLevel);
+    public Task Play(string accountId, int heroId);
+    public Task<HeroGet> GetInGameHero(string accountId);
+}
---
+++ Server/Database/Repository/HeroMenager.cs
@@ -0,0 +1,108 @@
+﻿using AutoMapper;
+using Classes.Exceptions;
+using Classes.Models.Game.Hero;
+using Classes.Enums.Game;
+using Database.Contracts;
+using Microsoft.EntityFrameworkCore;
+using Classes.Statics.Game;
+using Classes.Exceptions.Game;
+using AutoMapper.QueryableExtensions;
+
+namespace Database.Repository;
+
+public class HeroMenager : GenericRepository<DBHero>, IHeroMenager
+{
+    private readonly IMapper _mapper;
+    private readonly DatabaseContext _context;
+
+    public HeroMenager(DatabaseContext _context, IMapper _mapper) : base(_context, _mapper)
+    {
+        this._mapper = _mapper;
+        this._context = _context;
+    }
+
+    public async Task<bool> IsAvailableNickname(string nickname)
+    {
+        return !await _context.Heroes.AnyAsync(hero => hero.Name == nickname);
+    }
+
+    public async Task<bool> IsHeroLimitReached(string accountId)
+    {
+        return await _context.Heroes.CountAsync(hero => hero.UserId == accountId) > 3;
+    }
+
+    public async Task<bool> IsHeroThisUser(string accountId, int heroId)
+    {
+        var hero = await _context.Heroes.FirstOrDefaultAsync(hero => hero.Id == heroId);
+
+        if (hero is null) throw new NotFoundException("Hero", heroId.ToString());
+
+        return hero.UserId == accountId;
+    }
+
+    public async Task<List<HeroBasicInfo>> GetHeroes(string userId)
+    {
+        return await _context.Heroes.ProjectTo<HeroBasicInfo>(_mapper.ConfigurationProvider).ToListAsync();
+    }
+
+    public async Task ChangeSprite(int heroId, int spriteLevel)
+    {
+        if (spriteLevel % 10 != 0) throw new BadRequestException("Incorrect sprite level. Only dozens are allowed.");
+
+        var hero = await _context.Heroes.FirstOrDefaultAsync(hero => hero.Id == heroId);
+
+        if (hero is null) throw new NotFoundException("Hero", heroId.ToString());
+
+        if (hero.Level < spriteLevel) throw new HeroLevelTooLowException(spriteLevel);
+
+        try
+        {
+            switch (hero.Profession)
+            {
+                case Profession.Warrior:
+                    if (hero.Sex == Sex.Male) hero.SpriteURL = Sprites.warriorMale[spriteLevel / 10];
+                    else hero.SpriteURL = Sprites.warriorFemale[spriteLevel / 10];
+                    break;
+                case Profession.Hunter:
+                    if (hero.Sex == Sex.Male) hero.SpriteURL = Sprites.hunterMale[spriteLevel / 10];
+                    else hero.SpriteURL = Sprites.hunterFemale[spriteLevel / 10];
+                    break;
+                case Profession.Mage:
+                    if (hero.Sex == Sex.Male) hero.SpriteURL = Sprites.mageMale[spriteLevel / 10];
+                    else hero.SpriteURL = Sprites.mageFemale[spriteLevel / 10];
+                    break;
+                case Profession.Paladin:
+                    if (hero.Sex == Sex.Male) hero.SpriteURL = Sprites.paladinMale[spriteLevel / 10];
+                    else hero.SpriteURL = Sprites.paladinFemale[spriteLevel / 10];
+                    break;
+            }
+        }
+        catch
+        {
+            throw new NotFoundException("Sprite", spriteLevel.ToString());
+        }
+
+        await Update(hero.Id, hero);
+    }
+
+    public async Task Play(string accountId, int heroId)
+    {
+        var heroes = await _context.Heroes.Where(hero => hero.UserId == accountId).ToListAsync();
+
+        if (!heroes.Any(hero => hero.Id == heroId)) throw new NotFoundException("Hero", heroId.ToString());
+
+        heroes.ForEach(hero =>
+        {
+            if (hero.Id == heroId) hero.InGame = true;
+            else hero.InGame = false;
+            _context.Update(hero);
+        });
+
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<HeroGet> GetInGameHero(string accountId)
+    {
+        return _mapper.Map<HeroGet>(await _context.Heroes.FirstOrDefaultAsync(hero => hero.UserId == accountId && hero.InGame == true));
+    }
+}
