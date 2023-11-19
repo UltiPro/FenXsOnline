6afe310a810e69d2ff11fa4ae3c4fb0c884d79b3
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-11-19 15:19:43 +0100

    Added Item Types

---
+++ Server/Classes/Enums/Game/ItemType.cs
@@ -0,0 +1,16 @@
+﻿namespace Classes.Enums.Game;
+
+public enum ItemType
+{
+    Weapon = 0,
+    SecondaryWeapon = 1,
+    Armor = 2,
+    Helmet = 3,
+    Boots = 4,
+    Gloves = 5,
+    Necklace = 6,
+    Ring = 7,
+    Consumables = 8,
+    Neutral = 9,
+    ToQuest = 10
+}
