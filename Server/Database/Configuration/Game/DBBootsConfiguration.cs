c84198f8f2accec3dc04f3c20d146e294c92ef8b
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-11-19 22:37:09 +0100

    Added DBBoots data

---
+++ Server/Database/Configuration/Game/DBBootsConfiguration.cs
@@ -0,0 +1,56 @@
+﻿using Classes.Enums.Game;
+using Classes.Models.Game.Item.Boots;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Database.Configuration.Game;
+
+public class DBBootsConfiguration : IEntityTypeConfiguration<DBBoots>
+{
+    public void Configure(EntityTypeBuilder<DBBoots> builder)
+    {
+        builder.HasData(new DBBoots
+        {
+            Id = 1,
+            Name = "Chainmail Boots",
+            Weight = 2,
+            Price = 5,
+            SpriteURL = "bootsWarrior0.gif",
+            Armor = 5,
+            Agility = 5,
+            Profession = Profession.Warrior
+        }, new DBBoots
+        {
+            Id = 2,
+            Name = "Sheepskin Boots",
+            Weight = 1,
+            Price = 1,
+            SpriteURL = "bootsHunter0.gif",
+            Armor = 1,
+            Agility = 5,
+            HealthPoints = 5,
+            Profession = Profession.Hunter
+        }, new DBBoots
+        {
+            Id = 3,
+            Name = "Leather Slippers",
+            Weight = 1,
+            Price = 2,
+            SpriteURL = "bootsMage0.gif",
+            MagicArmor = 5,
+            Agility = 2,
+            HealthPoints = 3,
+            Profession = Profession.Mage
+        }, new DBBoots
+        {
+            Id = 4,
+            Name = "Steel Boots",
+            Weight = 3,
+            Price = 10,
+            SpriteURL = "bootsPaladin0.gif",
+            Armor = 15,
+            MagicArmor = 5,
+            Profession = Profession.Paladin
+        });
+    }
+}
