46d586f410e3bcfcc67d85665e27a90939bee425
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-11-20 19:56:36 +0100

    Json ignore for controller

--- Server/Classes/Models/Game/Item/BaseItemWornable.cs
+++ Server/Classes/Models/Game/Item/BaseItemWornable.cs
@@ -1,4 +1,5 @@
 ﻿using Classes.Models.Game.Hero;
+using System.Text.Json.Serialization;
 
 namespace Classes.Models.Game.Item;
 
@@ -7,5 +8,6 @@ public class BaseItemWornable : BaseItem
     public int Level { get; set; } = 1;
     public int Weight { get; set; } = 1;
     public int Price { get; set; } = 1;
+    [JsonIgnore]
     public ICollection<DBHero>? Heroes { get; set; }
 }
