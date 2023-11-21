257e47b16f73f389be2bfb5491ef10e106465d11
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-11-21 15:06:44 +0100

    Updated hero models

--- Server/Classes/Models/Game/Hero/DBHero.cs
+++ Server/Classes/Models/Game/Hero/DBHero.cs
@@ -8,6 +8,7 @@ using Classes.Models.Game.Item.SecondaryWeapon;
 using Classes.Models.Game.Item.Weapon;
 using Classes.Models.User;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace Classes.Models.Game.Hero;
 
@@ -16,47 +17,61 @@ public class DBHero : HeroBasicInfo
     public int Atack { get; set; } = 0;
     public int MagicAtack { get; set; } = 0;
     public int CriticalChance { get; set; } = 0;
-    public int Armor { get; set; } = 1;
+    public int Armor { get; set; } = 0;
     public int MagicArmor { get; set; } = 0;
     public int Agility { get; set; } = 0;
-    public int Weight { get; set; } = 1;
+    public int Weight { get; set; } = 0;
     public int Gold { get; set; } = 0;
     public int MapId { get; set; } = 0;
     public int X { get; set; } = 3;
     public int Y { get; set; } = 2;
+    [JsonIgnore]
     public bool InGame { get; set; } = false;
-    public bool IsDead { get; set; } = false;
+    public DateTime? Dead { get; set; } = null;
 
-    // Account
+    [JsonIgnore]
     public string UserId { get; set; }
     [ForeignKey("UserId")]
+    [JsonIgnore]
     public DBUser DBUser { get; set; }
 
-    // Foreigns Ids
+    [JsonIgnore]
     public int? ArmorId { get; set; } = null;
-    public int? BootsId { get; set; } = null;
-    public int? GlovesId { get; set; } = null;
-    public int? HelmetId { get; set; } = null;
-    public int? NecklaceId { get; set; } = null;
-    public int? RingId { get; set; } = null;
-    public int? SecondaryWeaponId { get; set; } = null;
-    public int? WeaponId { get; set; } = null;
-
-    // Foreigns Objects
     [ForeignKey("ArmorId")]
     public DBArmor? DBArmor { get; set; } = null;
+
+    [JsonIgnore]
+    public int? BootsId { get; set; } = null;
     [ForeignKey("BootsId")]
     public DBBoots? DBBoots { get; set; } = null;
+
+    [JsonIgnore]
+    public int? GlovesId { get; set; } = null;
     [ForeignKey("GlovesId")]
     public DBGloves? DBGloves { get; set; } = null;
+
+    [JsonIgnore]
+    public int? HelmetId { get; set; } = null;
     [ForeignKey("HelmetId")]
     public DBHelmet? DBHelmet { get; set; } = null;
+
+    [JsonIgnore]
+    public int? NecklaceId { get; set; } = null;
     [ForeignKey("NecklaceId")]
     public DBNecklace? DBNecklace { get; set; } = null;
+
+    [JsonIgnore]
+    public int? RingId { get; set; } = null;
     [ForeignKey("RingId")]
     public DBRing? DBRing { get; set; } = null;
+
+    [JsonIgnore]
+    public int? SecondaryWeaponId { get; set; } = null;
     [ForeignKey("SecondaryWeaponId")]
     public DBSecondaryWeapon? DBSecondaryWeapon { get; set; } = null;
+
+    [JsonIgnore]
+    public int? WeaponId { get; set; } = null;
     [ForeignKey("WeaponId")]
     public DBWeapon? DBWeapon { get; set; } = null;
 }
