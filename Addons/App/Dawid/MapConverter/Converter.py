edd3dc7c94315f1a1ee73e76caad301f09a10df2
Dawid Majewski <80513@student.pb.edu.pl>
2023-11-21 07:43:38 +0100

    Collisions Mapper - fix
    Fix for larger maps
    tested on map with 6144 fields

--- Addons/App/Dawid/MapConverter/Converter.py
+++ Addons/App/Dawid/MapConverter/Converter.py
@@ -2,52 +2,48 @@ from PIL import Image, ImageSequence
 import tkinter as tk
 from tkinter import filedialog
 
-def check_color_block(data, x, y, width, height):
+def check_color_block(img, x, y):
     green_threshold = 200  # Adjust this threshold as needed
     red_threshold = 150  # Adjust this threshold as needed
-
+    
     green_count = 0
     red_count = 0
 
-    for i in range(x, min(x + 32, width)):
-        for j in range(y, min(y + 32, height)):
-            pixel_index = j * width + i
-            pixel = data[pixel_index]
-            r, g, b = pixel[0], pixel[1], pixel[2]  # Extract RGB values
-            if g > green_threshold and r < red_threshold:
-                green_count += 1
-            elif r > red_threshold and g < green_threshold:
-                red_count += 1
-
+    for i in range(x, x + 32):
+        for j in range(y, y + 32):
+            if i < img.width and j < img.height:
+                pixel = img.getpixel((i, j))
+                if isinstance(pixel, int):  # For grayscale images
+                    if pixel > green_threshold:
+                        green_count += 1
+                    elif pixel < red_threshold:
+                        red_count += 1
+                else:  # For RGB images
+                    r, g, b = pixel[:3]
+                    if g >= green_threshold and r < red_threshold:
+                        green_count += 1
+                    elif r >= red_threshold and g < green_threshold:
+                        red_count += 1
+    
     return green_count > red_count
 
 def process_image(image_path):
     img = Image.open(image_path)
-
-    # If the image is a GIF, take the first frame as a static image
-    if img.is_animated:
-        frames = list(ImageSequence.Iterator(img))
-        img = frames[0]
-
-    width, height = img.size
-    data = list(img.getdata())
-
-    rows = height // 32
-    cols = width // 32
+    rows = img.height // 32
+    cols = img.width // 32
 
     output_data = []
-    for y in range(0, height, 32):
-        for x in range(0, width, 32):
-            is_green = check_color_block(data, x, y, width, height)
-            output_data.append(str(is_green))
 
-    # Reshape data into rows and cols
-    reshaped_data = [output_data[i:i + cols] for i in range(0, len(output_data), cols)]
+    for y in range(0, img.height, 32):
+        row_data = []
+        for x in range(0, img.width, 32):
+            is_green = check_color_block(img, x, y)
+            row_data.append(str(is_green))
+        output_data.append(' '.join(row_data))
 
     # Save the boolean values to a text file with rows and cols
     with open('output.txt', 'w') as file:
-        for row in reshaped_data:
-            file.write(' '.join(row) + '\n')
+        file.write('\n'.join(output_data))
 
 def select_file():
     root = tk.Tk()
