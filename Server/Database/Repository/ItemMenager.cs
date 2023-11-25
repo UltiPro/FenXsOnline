85cf890281823ef880ed27afa43592f33c292951
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-11-25 20:31:33 +0100

    Added item menager logic

---
+++ Server/Database/Repository/ItemMenager.cs
@@ -0,0 +1,23 @@
+﻿using Classes.Exceptions;
+using Database.Contracts;
+
+namespace Database.Repository;
+
+public class ItemMenager : IItemMenager
+{
+    private readonly DatabaseContext _context;
+    public ItemMenager(DatabaseContext _context)
+    {
+        this._context = _context;
+    }
+    public async Task<T> Get<T>(int id) where T : class
+    {
+        if (id < 1) throw new BadRequestException("Item id needs to be larger than 0.");
+
+        object? item = await _context.Set<T>().FindAsync(id);
+
+        if (item is null) throw new NotFoundException(typeof(T).Name, id);
+
+        return (T)item;
+    }
+}
