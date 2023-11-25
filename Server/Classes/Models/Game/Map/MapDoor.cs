28bfacf4079472270b37edb8d0bc9eef6d8db469
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-11-25 23:05:30 +0100

    Update MapDoor.cs

--- Server/Classes/Models/Game/Map/MapDoor.cs
+++ Server/Classes/Models/Game/Map/MapDoor.cs
@@ -2,6 +2,8 @@
 
 public class MapDoor
 {
+    public int FromX { get; set; }
+    public int FromY { get; set; }
     public int ToMapId { get; set; }
     public int ToX { get; set; }
     public int ToY { get; set; }
