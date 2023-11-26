a5e6a737c6974f2141c69d8f94c2968f1bd410b4
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-11-27 00:23:56 +0100

    Create IEquipmentMenager.cs

---
+++ Server/Database/Contracts/IEquipmentMenager.cs
@@ -0,0 +1,12 @@
+﻿using Classes.Enums.Game;
+
+namespace Database.Contracts;
+
+public interface IEquipmentMenager
+{
+    Task Create(int heroId);
+    Task Delete(int heroId);
+    Task GetItem(int heroId, ItemType itemType, int itemId);
+    Task MoveItem(int heroId, int fromSlotId, int toSlotId);
+    Task DestroyItem(int heroId, int itemId);
+}
