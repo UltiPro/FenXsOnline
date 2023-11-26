3c2ac745dd7fc9bd14622904c58a3eb3ff0aabef
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-11-26 01:49:05 +0100

    Correct Model

--- Server/Classes/Models/Game/Hero/DBHeroEquipment.cs
+++ Server/Classes/Models/Game/Hero/DBHeroEquipment.cs
@@ -1,15 +1,17 @@
-﻿using Classes.Models.Game.Item;
+﻿using Classes.Enums.Game;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
 
 namespace Classes.Models.Game.Hero;
 
-public class DBHeroEquipment : ItemProvider
+public class DBHeroEquipment
 {
-    public int OrderId { get; set; }
     [JsonIgnore]
-    public string HeroId { get; set; }
+    public int HeroId { get; set; }
     [ForeignKey("HeroId")]
     [JsonIgnore]
     public DBHero DBHero { get; set; }
+    public int OrderId { get; set; }
+    public ItemType? ItemType { get; set; } = null;
+    public int? ItemId { get; set; } = null;
 }
