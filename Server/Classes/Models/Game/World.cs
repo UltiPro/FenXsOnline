739c7e8b27d7d8736ea09b7f62b9b49bd61c9443
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-11-28 15:44:51 +0100

    Update World.cs

--- Server/Classes/Models/Game/World.cs
+++ Server/Classes/Models/Game/World.cs
@@ -1,4 +1,5 @@
 ﻿using Classes.Models.Game.Map;
+using Classes.Models.Game.Map.Instance;
 
 namespace Classes.Models.Game;
 
@@ -7,49 +8,13 @@ public sealed class World
     private static World _world;
 
     public readonly List<MapData> Maps = new List<MapData>();
-    private readonly bool x = false;
-    private readonly bool o = true;
 
     private World()
     {
-        Maps.Add(new MapData
-        {
-            Id = 0,
-            Name = "Home",
-            SpriteURL = "",
-            IsPvP = false,
-            Fields = new bool[11, 13]
-            {
-                {x, x, x, x, x, x, x, x, x, x, x, x, x },
-                {x, x, x, x, x, x, x, x, x, x, x, x, x },
-                {x, x, x, x, x, x, x, x, x, x, x, x, x },
-                {x, x, o, o, x, o, o, o, o, o, o, o, x },
-                {x, o, o, o, o, x, o, o, x, x, x, x, x },
-                {x, o, o, o, x, x, o, o, x, x, x, x, x },
-                {x, x, o, o, x, x, o, o, o, x, x, x, x },
-                {x, x, o, o, o, o, o, o, o, o, o, o, x },
-                {x, x, o, o, o, o, o, o, o, o, o, o, x },
-                {x, x, x, x, x, x, x, o, o, x, x, x, x },
-                {x, x, x, x, x, x, x, x, x, x, x, x, x }
-            },
-            Doors = new List<MapDoor>
-            {
-                new MapDoor{
-                    FromX = 7,
-                    FromY = 9,
-                    ToMapId = 1,
-                    ToX = 1,
-                    ToY = 1
-                },
-                new MapDoor{
-                    FromX = 8,
-                    FromY = 9,
-                    ToMapId = 1,
-                    ToX = 1,
-                    ToY = 1
-                }
-            }
-        });
+        Maps.Add(Map0.Get());
+        Maps.Add(Map1.Get());
+        Maps.Add(Map2.Get());
+        Maps.Add(Map3.Get());
     }
 
     public static World GetInstance()
