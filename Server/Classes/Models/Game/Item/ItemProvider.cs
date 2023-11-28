b7ea2b1c985ebf62c3aaf0f249db760a903cf214
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-11-28 15:27:06 +0100

    Added Items providers

---
+++ Server/Classes/Models/Game/Item/ItemProvider.cs
@@ -0,0 +1,9 @@
+﻿using Classes.Enums.Game;
+
+namespace Classes.Models.Game.Item;
+
+public class ItemProvider
+{
+    public ItemType ItemType { get; set; }
+    public int Id { get; set; }
+}
---
+++ Server/Classes/Models/Game/Item/ItemProviderTimer.cs
@@ -0,0 +1,6 @@
+﻿namespace Classes.Models.Game.Item;
+
+public class ItemProviderTimer : ItemProvider
+{
+    DateTime Avaliable { get; set; }
+}
