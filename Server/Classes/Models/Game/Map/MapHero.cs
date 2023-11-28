b657b9c0d9b2767690ad868d565651e585331c31
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-11-28 16:02:41 +0100

    Update MapHero.cs

--- Server/Classes/Models/Game/Map/MapHero.cs
+++ Server/Classes/Models/Game/Map/MapHero.cs
@@ -6,7 +6,7 @@ public class MapHero
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public int Level { get; set; } = 1;
+    public int Level { get; set; }
     public Profession Profession { get; set; }
     public Sex Sex { get; set; }
     public string SpriteURL { get; set; }
