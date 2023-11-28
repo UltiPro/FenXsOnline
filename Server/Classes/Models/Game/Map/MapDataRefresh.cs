427c13ff7a35b82bd3f41b989f387e43c6fa4a6f
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-11-28 18:46:09 +0100

    Added map data refresh model

---
+++ Server/Classes/Models/Game/Map/MapDataRefresh.cs
@@ -0,0 +1,11 @@
+﻿using Classes.Models.Game.Item;
+
+namespace Classes.Models.Game.Map;
+
+public class MapDataRefresh
+{
+    public List<MapMob>? Mobs { get; set; } = null;
+    public List<ItemProviderTimer>? Items { get; set; } = null;
+    public List<ItemProvider>? Dropped_Items { get; set; } = null;
+    public List<MapHero>? Heroes { get; set; } = null;
+}
