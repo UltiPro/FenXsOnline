2d16e37b6556642b4a66f2dab998c0c2ca4758c9
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-11-28 18:46:23 +0100

    Added map npc model (to edit)

--- Server/Classes/Models/Game/Map/MapNPC.cs
+++ Server/Classes/Models/Game/Map/MapNPC.cs
@@ -7,6 +7,7 @@ public class MapNPC : MapPosition
     public int Id { get; set; }
     public string Name { get; set; }
     public string SpriteURL { get; set; }
+    public bool IsHealer { get; set; }
     public bool IsTrader { get; set; }
     public int? PricePercent { get; set; } = null;
     public List<ItemProvider>? Items { get; set; } = null;
