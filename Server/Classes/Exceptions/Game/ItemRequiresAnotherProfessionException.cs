f46df58c2886c1e2b3ac24a4661f20874b7ff50f
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-12-07 20:13:15 +0100

    Added new exceptions

---
+++ Server/Classes/Exceptions/Game/ItemRequiresAnotherProfessionException.cs
@@ -0,0 +1,6 @@
+﻿namespace Classes.Exceptions.Game;
+
+public class ItemRequiresAnotherProfessionException : Exception
+{
+    public ItemRequiresAnotherProfessionException(string professionName) : base($"Item requires {professionName} profession.") { }
+}
