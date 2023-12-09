c419e644000a5d3943b65573bcc6f0400c0e06b3
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-12-09 22:30:55 +0100

    Refactor

--- Server/Classes/Statics/Role.cs
+++ Server/Classes/Statics/Role.cs
@@ -3,9 +3,6 @@
 public static class Role
 {
     public static readonly string User = "User";
-    public static readonly string UserN = "USER";
     public static readonly string Mod = "Moderator";
-    public static readonly string ModN = "MODERATOR";
     public static readonly string Admin = "Administrator";
-    public static readonly string AdminN = "ADMINISTRATOR";
 }
