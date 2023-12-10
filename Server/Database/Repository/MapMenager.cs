b3c0bcb4de2469d890d581bd32275c4c66793095
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-12-10 17:52:31 +0100

    Update MapMenager.cs

--- Server/Database/Repository/MapMenager.cs
+++ Server/Database/Repository/MapMenager.cs
@@ -1,27 +1,72 @@
-﻿using Classes.Models.Game.Map;
+﻿using AutoMapper;
+using Classes.Exceptions.Game;
+using Classes.Models.Game;
+using Classes.Models.Game.Hero;
+using Classes.Models.Game.Item;
+using Classes.Models.Game.Map;
+using Classes.Enums.Game;
 using Database.Contracts;
+using Microsoft.EntityFrameworkCore;
 
 namespace Database.Repository;
 
 public class MapMenager : IMapMenager
 {
-    public Task<bool> DropItem(int accountId, int itemId)
+    private readonly DatabaseContext _context;
+    private readonly IMapper _mapper;
+    private readonly World _world;
+
+    public MapMenager(DatabaseContext _context)
+    {
+        this._context = _context;
+        _world = World.GetInstance();
+    }
+
+    public async Task<MapData> Get(string accountId)
+    {
+        return _world.Maps[(await GetHero(accountId)).MapId];
+    }
+
+    public async Task<MapDataRefresh> GetRefresh(string accountId)
+    {
+        return _mapper.Map<MapDataRefresh>(_world.Maps[(await GetHero(accountId)).MapId]);
+    }
+
+    public async Task DropItem(string accountId, int itemId)
+    {
+        var hero = await GetHero(accountId);
+
+        var slot = hero.HeroEquipment.FirstOrDefault(slot => slot.Id == itemId);
+
+        if (slot.ItemType is null || slot.ItemId is null) throw new HeroEquipmentSlotIsEmptyException();
+
+        if (slot.ItemType is ItemType.ToQuest) throw new ItemIsNotDroppableException();
+
+        _world.Maps[hero.MapId].Items.Add(new ItemProviderGround
+        {
+            ItemType = (ItemType)slot.ItemType,
+            ItemId = (int)slot.ItemId,
+            X = hero.X,
+            Y = hero.Y
+        });
+
+        slot.ItemType = null;
+        slot.ItemId = null;
+
+        await _context.SaveChangesAsync();
+    }
+
+    public Task<ItemProvider> GrabItem(string accountId)
     {
         throw new NotImplementedException();
     }
 
-    public Task<MapDataResponse> Get(int id)
+    private async Task<DBHero> GetHero(string accountId)
     {
-        throw new NotImplementedException();
-    }
+        var hero = await _context.Heroes.FirstOrDefaultAsync(hero => hero.UserId == accountId && hero.InGame);
 
-    public Task<List<MapHero>> GetMapHeroes()
-    {
-        throw new NotImplementedException();
-    }
+        if (hero is null) throw new HeroIsNotInTheGameException();
 
-    public Task<bool> GrabItem(int accountId)
-    {
-        throw new NotImplementedException();
+        return hero;
     }
 }
