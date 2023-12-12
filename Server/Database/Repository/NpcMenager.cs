f03dd3c2e9730637a00eade1692aea07e8d9f34e
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-12-12 21:53:00 +0100

    Added shop and healing logic

---
+++ Server/Database/Repository/NpcMenager.cs
@@ -0,0 +1,176 @@
+﻿using Classes.Enums.Game;
+using Classes.Exceptions;
+using Classes.Exceptions.Game;
+using Classes.Models.Game;
+using Classes.Models.Game.Hero;
+using Classes.Models.Game.Item;
+using Classes.Models.Game.Item.Armor;
+using Classes.Models.Game.Item.Boots;
+using Classes.Models.Game.Item.Consumables;
+using Classes.Models.Game.Item.Gloves;
+using Classes.Models.Game.Item.Helmet;
+using Classes.Models.Game.Item.Necklace;
+using Classes.Models.Game.Item.Neutral;
+using Classes.Models.Game.Item.Ring;
+using Classes.Models.Game.Item.SecondaryWeapon;
+using Classes.Models.Game.Item.Weapon;
+using Classes.Models.Game.Map;
+using Database.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database.Repository;
+
+public class NpcMenager : INpcMenager
+{
+    private readonly DatabaseContext _context;
+    private readonly IEquipmentMenager _equipmentMenager;
+    private readonly IItemMenager _itemMenager;
+    private readonly World _world;
+
+    public NpcMenager(DatabaseContext _context, IEquipmentMenager _equipmentMenager, IItemMenager _itemMenager)
+    {
+        this._context = _context;
+        this._equipmentMenager = _equipmentMenager;
+        this._itemMenager = _itemMenager;
+        _world = World.GetInstance();
+    }
+
+    public async Task Heal(string accountId, int npcId)
+    {
+        var hero = await GetHero(accountId);
+
+        var npc = GetNpc(hero, npcId);
+
+        if (npc.IsHealer)
+        {
+            hero.HealthPoints = hero.MaxHealthPoints;
+
+            await _context.SaveChangesAsync();
+
+            return;
+        }
+
+        throw new BadRequestException("This NPC is not a healer.");
+    }
+
+    public async Task<int> Sell(string accountId, int npcId, int itemId)
+    {
+        var hero = await GetHero(accountId);
+
+        var npc = GetNpc(hero, npcId);
+
+        if (npc.IsTrader)
+        {
+            var slot = await _context.HeroesEquipments.FirstOrDefaultAsync(slot => slot.DBHero == hero && slot.Id == itemId);
+
+            if (slot is null || slot.ItemType is null || slot.ItemId is null) throw new HeroEquipmentSlotIsEmptyException();
+
+            var item = await GetBaseItem((ItemType)slot.ItemType, (int)slot.ItemId);
+
+            var coins = (int)(item.Price * npc.PricePercent);
+
+            hero.Gold += coins;
+
+            slot.ItemType = null;
+            slot.ItemId = null;
+
+            await _context.SaveChangesAsync();
+
+            return coins;
+        }
+
+        throw new BadRequestException("This NPC is not a trader.");
+    }
+
+    public async Task<DBHeroEquipment> Buy(string accountId, int npcId, int itemId)
+    {
+        var hero = await GetHero(accountId);
+
+        var npc = GetNpc(hero, npcId);
+
+        if (npc.IsTrader)
+        {
+            var itemNpc = npc.Items.FirstOrDefault(item => item.Id == itemId);
+
+            if (itemNpc is null) throw new BadRequestException("This npc do not sell item in provided slot.");
+
+            var item = await GetBaseItem(itemNpc.ItemType, itemNpc.ItemId);
+
+            if (hero.Gold < item.Price) throw new HeroHasNotEnoughGoldException();
+
+            var itemEq = await _equipmentMenager.AddItem(accountId, itemNpc);
+
+            hero.Gold -= item.Price;
+
+            await _context.SaveChangesAsync();
+
+            return itemEq;
+        }
+
+        throw new BadRequestException("This NPC is not a trader.");
+    }
+
+    private async Task<DBHero> GetHero(string accountId)
+    {
+        var hero = await _context.Heroes.FirstOrDefaultAsync(hero => hero.UserId == accountId && hero.InGame);
+
+        if (hero is null) throw new HeroIsNotInTheGameException();
+
+        return hero;
+    }
+
+    private MapNPC GetNpc(DBHero hero, int npcId)
+    {
+        var npc = _world.Maps[hero.MapId].NPCs.FirstOrDefault(npc => npc.Id == npcId);
+
+        if (npc is null) throw new NotFoundException("NPC", npcId);
+
+        if (Math.Sqrt(Math.Pow(npc.X - hero.X, 2) + Math.Pow(npc.Y - hero.Y, 2)) > 2)
+            throw new HeroIsTooFarAwayException();
+
+        return npc;
+    }
+
+    private async Task<BaseItem> GetBaseItem(ItemType itemType, int itemId)
+    {
+        BaseItem item;
+
+        switch (itemType)
+        {
+            case ItemType.Armor:
+                item = await _itemMenager.GetBaseItem<DBArmor>(itemId);
+                break;
+            case ItemType.Boots:
+                item = await _itemMenager.GetBaseItem<DBBoots>(itemId);
+                break;
+            case ItemType.Gloves:
+                item = await _itemMenager.GetBaseItem<DBGloves>(itemId);
+                break;
+            case ItemType.Helmet:
+                item = await _itemMenager.GetBaseItem<DBHelmet>(itemId);
+                break;
+            case ItemType.Necklace:
+                item = await _itemMenager.GetBaseItem<DBNecklace>(itemId);
+                break;
+            case ItemType.Ring:
+                item = await _itemMenager.GetBaseItem<DBRing>(itemId);
+                break;
+            case ItemType.SecondaryWeapon:
+                item = await _itemMenager.GetBaseItem<DBSecondaryWeapon>(itemId);
+                break;
+            case ItemType.Weapon:
+                item = await _itemMenager.GetBaseItem<DBWeapon>(itemId);
+                break;
+            case ItemType.Consumables:
+                item = await _itemMenager.GetBaseItem<DBConsumables>(itemId);
+                break;
+            case ItemType.Neutral:
+                item = await _itemMenager.GetBaseItem<DBNeutral>(itemId);
+                break;
+            default:
+                throw new ItemIsNotSellableException();
+        }
+
+        return item;
+    }
+}
---
+++ Server/Server/Controllers/NpcController.cs
@@ -0,0 +1,73 @@
+﻿using Database.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class NpcController : ControllerBase
+{
+    private readonly IAuthMenager _authMenager;
+    private readonly IConfiguration _configuration;
+    private readonly INpcMenager _npcMenager;
+
+    public NpcController(IConfiguration _configuration, IAuthMenager _authMenager, INpcMenager _npcMenager)
+    {
+        this._configuration = _configuration;
+        this._authMenager = _authMenager;
+        this._npcMenager = _npcMenager;
+    }
+
+    [HttpPut]
+    [Route("heal")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult> Heal(int npcId)
+    {
+        var cookieId = HttpContext.Request.Cookies[_configuration["JwtSettings:IdCookie"]] ?? "";
+
+        await _authMenager.VerifyId(cookieId, HttpContext.Request.Cookies[_configuration["JwtSettings:TokenCookie"]] ?? "");
+
+        await _npcMenager.Heal(cookieId, npcId);
+
+        return NoContent();
+    }
+
+    [HttpPut]
+    [Route("sell")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult> Sell(int npcId, int itemId)
+    {
+        var cookieId = HttpContext.Request.Cookies[_configuration["JwtSettings:IdCookie"]] ?? "";
+
+        await _authMenager.VerifyId(cookieId, HttpContext.Request.Cookies[_configuration["JwtSettings:TokenCookie"]] ?? "");
+
+        return Ok(await _npcMenager.Sell(cookieId, npcId, itemId));
+    }
+
+    [HttpPut]
+    [Route("buy")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult> Buy(int npcId, int itemId)
+    {
+        var cookieId = HttpContext.Request.Cookies[_configuration["JwtSettings:IdCookie"]] ?? "";
+
+        await _authMenager.VerifyId(cookieId, HttpContext.Request.Cookies[_configuration["JwtSettings:TokenCookie"]] ?? "");
+
+        return Ok(await _npcMenager.Buy(cookieId, npcId, itemId));
+    }
+}
