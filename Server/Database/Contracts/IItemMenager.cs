a8cc0b723db25b64a318cc75a9cbcc7dfcc5ad74
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-12-12 17:00:29 +0100

    Update IItemMenager.cs

--- Server/Database/Contracts/IItemMenager.cs
+++ Server/Database/Contracts/IItemMenager.cs
@@ -1,6 +1,9 @@
-﻿namespace Database.Contracts;
+﻿using Classes.Models.Game.Item;
+
+namespace Database.Contracts;
 
 public interface IItemMenager
 {
     Task<T> Get<T>(int id) where T : class;
+    Task<BaseItem> GetBaseItem<T>(int id) where T : class;
 }
