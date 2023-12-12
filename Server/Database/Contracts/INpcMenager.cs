fa5df1c2769d8619fd1ecffbc4fdbe3b66ac2fca
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-12-12 17:00:58 +0100

    Added npc menager interface

---
+++ Server/Database/Contracts/INpcMenager.cs
@@ -0,0 +1,10 @@
+﻿using Classes.Models.Game.Hero;
+
+namespace Database.Contracts;
+
+public interface INpcMenager
+{
+    Task<bool> Heal(string accountId, int npcId);
+    Task<int> Sell(string accountId, int npcId, int itemId);
+    Task<DBHeroEquipment> Buy(string accountId, int npcId, int itemId);
+}
