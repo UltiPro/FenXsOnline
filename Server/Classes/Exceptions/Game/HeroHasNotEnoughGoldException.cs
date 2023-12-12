609810e88eab3fdb93af5e2d361a621d8529e7ae
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-12-12 18:45:54 +0100

    Added new exception and handling

---
+++ Server/Classes/Exceptions/Game/HeroHasNotEnoughGoldException.cs
@@ -0,0 +1,6 @@
+﻿namespace Classes.Exceptions.Game;
+
+public class HeroHasNotEnoughGoldException : Exception
+{
+    public HeroHasNotEnoughGoldException() : base("Hero has not enough gold to buy this item.") { }
+}
