f2f789170aaf1ee72a4d20485cbc8c4bcf52dd03
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-12-18 00:28:45 +0100

    DB map items

---
+++ Server/Database/Configuration/Game/Map/Item/DBMap2ItemConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Classes.Enums.Game;
+using Classes.Models.Game.Map;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Database.Configuration.Game.Map.Item;
+
+public class DBMap2ItemConfiguration : IEntityTypeConfiguration<DBMapItem>
+{
+    public void Configure(EntityTypeBuilder<DBMapItem> builder)
+    {
+        builder.HasData(new DBMapItem
+        {
+            MapId = 2,
+            X = 20,
+            Y = 12,
+            ItemType = ItemType.Consumables,
+            ItemId = 12,
+            Available = DateTime.Now
+        }, new DBMapItem
+        {
+            MapId = 2,
+            X = 1,
+            Y = 2,
+            ItemType = ItemType.Consumables,
+            ItemId = 12,
+            Available = DateTime.Now
+        });
+    }
+}
