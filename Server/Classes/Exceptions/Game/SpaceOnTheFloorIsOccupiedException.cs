25f9d502cedb5c05c15e7f73a854e7277a35df8c
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-12-17 22:24:07 +0100

    Rename

--- Server/Classes/Exceptions/Game/SpaceOnTheFloorIsOccupiedException.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Classes.Exceptions.Game;
-
-public class SpaceOnTheFloorIsOccupiedException : Exception
-{
-    public SpaceOnTheFloorIsOccupiedException() : base("This space on the floor is occupied.") { }
-}
---
+++ Server/Classes/Exceptions/Game/TileIsOccupiedException.cs
@@ -0,0 +1,6 @@
+﻿namespace Classes.Exceptions.Game;
+
+public class TileIsOccupiedException : Exception
+{
+    public TileIsOccupiedException() : base("This tile is occupied.") { }
+}
