6e93222ab61fb62cacdbe0d23d1c9d83a873a7f0
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-12-17 18:27:05 +0100

    -

--- Server/Classes/Models/Game/Item/ItemProviderGround.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Classes.Models.Game.Item;
-
-public class ItemProviderGround : ItemProvider
-{
-    public int X { get; set; }
-    public int Y { get; set; }
-    public DateTime? Available { get; set; } = null;
-}
