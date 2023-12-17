9b9db6dfb27d5b1a971b49a79210a8011e5ee7df
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-12-17 17:24:48 +0100

    Create DBMapMob.cs

---
+++ Server/Classes/Models/Game/Map/DBMapMob.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Classes.Models.Game.Map;
+
+public class DBMapMob
+{
+    [JsonIgnore]
+    public int MapId { get; set; }
+    public int MobId { get; set; }
+    public int X { get; set; }
+    public int Y { get; set; }
+}
