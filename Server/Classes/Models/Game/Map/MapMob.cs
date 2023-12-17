934a1e4f0775432531d07c30625ad0a7e9ac8691
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-12-17 16:30:23 +0100

    Added mobs models

--- Server/Classes/Models/Game/Map/MapMob.cs
+++ Server/Classes/Models/Game/Map/MapMob.cs
@@ -1,3 +1,6 @@
 ﻿namespace Classes.Models.Game.Map;
 
-public class MapMob { }
+public class MapMob : MapPosition
+{
+    public int Id { get; set; }
+}
---
+++ Server/Classes/Models/Game/Mob/MobProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace Classes.Models.Game.Mob;
+
+public class MobProvider
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string SpriteUrl { get; set; }
+    public int Level { get; set; }
+}
