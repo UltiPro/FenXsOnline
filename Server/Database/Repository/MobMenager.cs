3f1ba81fade30904fbfcf4d59fda55a5d603d8db
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-12-17 17:51:04 +0100

    Create MobMenager.cs

---
+++ Server/Database/Repository/MobMenager.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Classes.Exceptions;
+using Classes.Models.Game.Mob;
+using Database.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace Database.Repository;
+
+public class MobMenager : IMobMenager
+{
+    private readonly DatabaseContext _context;
+    private readonly IMapper _mapper;
+
+    public MobMenager(DatabaseContext _context, IMapper _mapper)
+    {
+        this._context = _context;
+        this._mapper = _mapper;
+    }
+
+    public async Task<DBMob> Get(int id)
+    {
+        var mob = await _context.Mobs
+            .Include(mob => mob.DropItems)
+            .FirstOrDefaultAsync(mob => mob.Id == id);
+
+        if (mob is null) throw new NotFoundException("Mob", id);
+
+        return mob;
+    }
+
+    public async Task<MobProvider> GetInfo(int id)
+    {
+        var mob = await _context.Mobs.FirstOrDefaultAsync(mob => mob.Id == id);
+
+        if (mob is null) throw new NotFoundException("Mob", id);
+
+        return _mapper.Map<MobProvider>(mob);
+    }
+}
