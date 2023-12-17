254af5210ed58693d7ec90035ae4584c0c0c027c
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-12-18 00:28:09 +0100

    Added db npc models

---
+++ Server/Classes/Models/Game/Npc/DBNpc.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Classes.Models.Game.Npc;
+
+public class DBNpc
+{
+    public int Id { get; set; }
+    [JsonIgnore]
+    public int MapId { get; set; }
+    public string Name { get; set; }
+    public string SpriteURL { get; set; }
+    public bool IsHealer { get; set; } = false;
+    public bool IsTrader { get; set; } = false;
+    public double PricePercent { get; set; } = 0.25;
+    public ICollection<DBNpcShopItem>? ShopItems { get; set; } = null;
+}
---
+++ Server/Classes/Models/Game/Npc/DBNpcShopItem.cs
@@ -0,0 +1,15 @@
+﻿using Classes.Models.Game.Item;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Classes.Models.Game.Npc;
+
+public class DBNpcShopItem : ItemProvider
+{
+    [JsonIgnore]
+    public int NpcId { get; set; }
+    [JsonIgnore]
+    [ForeignKey("NpcId")]
+    public DBNpc DBNpc { get; set; }
+    public int Id { get; set; }
+}
