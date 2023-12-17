638b4ac17105447ec0a3cdcb3df0dce2c810bc7c
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-12-17 18:28:11 +0100

    Update DBMapMob.cs

--- Server/Classes/Models/Game/Map/DBMapMob.cs
+++ Server/Classes/Models/Game/Map/DBMapMob.cs
@@ -9,4 +9,5 @@ public class DBMapMob
     public int MobId { get; set; }
     public int X { get; set; }
     public int Y { get; set; }
+    public DateTime Available { get; set; } = DateTime.Now;
 }
