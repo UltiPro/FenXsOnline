80ef25fd2a20b0b2cf807372b9478d79fff2edfc
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-12-17 17:53:00 +0100

    Create MobController.cs

---
+++ Server/Server/Controllers/MobController.cs
@@ -0,0 +1,25 @@
+﻿using Database.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class MobController : ControllerBase
+{
+    private readonly IMobMenager _mobMenager;
+
+    public MobController(IMobMenager _mobMenager)
+    {
+        this._mobMenager = _mobMenager;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult> Get(int id)
+    {
+        return Ok(await _mobMenager.GetInfo(id));
+    }
+}
