9a7c327485c6af33e5eb8c89e216d4d4551e79c5
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-12-18 01:21:59 +0100

    Added shop configurations

---
+++ Server/Database/Configuration/Game/Npc/DBNpc3ShopConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Classes.Enums.Game;
+using Classes.Models.Game.Npc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Database.Configuration.Game.Npc;
+
+public class DBNpc3ShopConfiguration : IEntityTypeConfiguration<DBNpcShopItem>
+{
+    public void Configure(EntityTypeBuilder<DBNpcShopItem> builder)
+    {
+        builder.HasData(
+            new DBNpcShopItem() { NpcId = 3, Id = 1, ItemType = ItemType.Ring, ItemId = 1 },
+            new DBNpcShopItem() { NpcId = 3, Id = 2, ItemType = ItemType.Ring, ItemId = 2 },
+            new DBNpcShopItem() { NpcId = 3, Id = 3, ItemType = ItemType.Ring, ItemId = 3 },
+            new DBNpcShopItem() { NpcId = 3, Id = 7, ItemType = ItemType.Necklace, ItemId = 1 },
+            new DBNpcShopItem() { NpcId = 3, Id = 8, ItemType = ItemType.Necklace, ItemId = 2 },
+            new DBNpcShopItem() { NpcId = 3, Id = 9, ItemType = ItemType.Necklace, ItemId = 3 }
+            );
+    }
+}
