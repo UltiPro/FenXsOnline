b747c906c0c1cc59a51d0a285b0f73ed296ba538
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-12-18 22:00:23 +0100

    Update models

--- Server/Classes/Models/Game/Mob/MobProvider.cs
+++ Server/Classes/Models/Game/Mob/MobProvider.cs
@@ -5,5 +5,5 @@ public class MobProvider
     public int Id { get; set; }
     public string Name { get; set; }
     public string SpriteUrl { get; set; }
-    public int Level { get; set; }
+    public int Level { get; set; } = 1;
 }
