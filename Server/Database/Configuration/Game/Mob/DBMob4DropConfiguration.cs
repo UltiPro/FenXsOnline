338085b55b9428b1bfea9830e4677e0097096463
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-12-18 01:50:28 +0100

    Rebuild

---
+++ Server/Database/Configuration/Game/Mob/DBMob4DropConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Classes.Enums.Game;
+using Classes.Models.Game.Mob;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Database.Configuration.Game.Mob;
+
+public class DBMob4DropConfiguration : IEntityTypeConfiguration<DBMobDrop>
+{
+    public void Configure(EntityTypeBuilder<DBMobDrop> builder)
+    {
+        builder.HasData(new DBMobDrop
+        {
+            MobId = 4,
+            ItemType = ItemType.Neutral,
+            ItemId = 2,
+        }, new DBMobDrop
+        {
+            MobId = 4,
+            ItemType = ItemType.Neutral,
+            ItemId = 3,
+        }, new DBMobDrop
+        {
+            MobId = 4,
+            ItemType = ItemType.Neutral,
+            ItemId = 4,
+        }, new DBMobDrop
+        {
+            MobId = 4,
+            ItemType = ItemType.Consumables,
+            ItemId = 11,
+        });
+    }
+}
