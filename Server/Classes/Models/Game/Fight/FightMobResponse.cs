e9dad61452313dfb84da0c831355a89f3721c0aa
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-12-19 23:01:10 +0100

    Create FightMobResponse.cs

---
+++ Server/Classes/Models/Game/Fight/FightMobResponse.cs
@@ -0,0 +1,15 @@
+﻿using Classes.Models.Game.Hero;
+
+namespace Classes.Models.Game.Fight;
+
+public class FightMobResponse
+{
+    public bool PlayerWin { get; set; }
+    public int HelathPoints { get; set; }
+    public DBHeroEquipment? Drop { get; set; }
+    public int MapId { get; set; }
+    public int X { get; set; }
+    public int Y { get; set; }
+    public DateTime? Dead { get; set; }
+    public List<string> Logs { get; set; }
+}
