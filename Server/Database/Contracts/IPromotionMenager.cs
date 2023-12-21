55511ed6ad85fc637eb89505ed4fb940e45149f2
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-12-21 21:04:01 +0100

    Create IPromotionMenager.cs

---
+++ Server/Database/Contracts/IPromotionMenager.cs
@@ -0,0 +1,8 @@
+﻿using Classes.Models.Game;
+
+namespace Database.Contracts;
+
+public interface IPromotionMenager
+{
+    Task<PromotionResponse> Promotion(string accountId, int level, bool quest);
+}
