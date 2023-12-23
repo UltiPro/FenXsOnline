e7f82ed8ab47c5bc7531b9326b3a997a9ff18f3f
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-12-23 04:18:35 +0100

    Improvement

--- Server/Database/Contracts/IPromotionMenager.cs
+++ Server/Database/Contracts/IPromotionMenager.cs
@@ -1,8 +1,9 @@
 ﻿using Classes.Models.Game;
+using Classes.Models.Game.Hero;
 
 namespace Database.Contracts;
 
 public interface IPromotionMenager
 {
-    Task<PromotionResponse> Promotion(string accountId, int level, bool quest);
+    Task<PromotionResponse> Promotion(DBHero hero, int level, bool quest);
 }
