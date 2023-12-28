28f0ff9730c019e7c8368638cda54445c684b3e1
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-12-28 21:16:45 +0100

    Added quest reward model and configuration

---
+++ Server/Classes/Models/Game/Quest/DBQuestReward.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+using Classes.Enums.Game;
+
+namespace Classes.Models.Game.Quest;
+
+public class DBQuestReward
+{
+    [JsonIgnore]
+    public int QuestId { get; set; }
+    [ForeignKey("QuestId")]
+    public DBQuest DBQuest { get; set; }
+    public ItemType ItemType { get; set; }
+    public int ItemId { get; set; }
+}
---
+++ Server/Database/Configuration/Game/Quest/DBQuestRewardConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Classes.Enums.Game;
+using Classes.Models.Game.Quest;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Database.Configuration.Game.Quest;
+
+public class DBQuestRewardConfiguration : IEntityTypeConfiguration<DBQuestReward>
+{
+    public void Configure(EntityTypeBuilder<DBQuestReward> builder)
+    {
+        builder.HasData(new DBQuestReward
+        {
+            QuestId = 1,
+            ItemType = ItemType.Consumables,
+            ItemId = 1
+        }, new DBQuestReward
+        {
+            QuestId = 4,
+            ItemType = ItemType.Neutral,
+            ItemId = 8
+        }, new DBQuestReward
+        {
+            QuestId = 5,
+            ItemType = ItemType.Neutral,
+            ItemId = 10
+        }, new DBQuestReward
+        {
+            QuestId = 5,
+            ItemType = ItemType.Neutral,
+            ItemId = 11
+        }, new DBQuestReward
+        {
+            QuestId = 5,
+            ItemType = ItemType.Neutral,
+            ItemId = 11
+        });
+    }
+}
