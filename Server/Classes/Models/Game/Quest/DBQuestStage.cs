5e453d4f4aa97d55bc9417d3ed8d80eb0e186f0f
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-12-28 20:11:27 +0100

    Add quest stages models and configuration

---
+++ Server/Classes/Models/Game/Quest/DBQuestStage.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+using Classes.Enums.Game;
+
+namespace Classes.Models.Game.Quest;
+
+public class DBQuestStage
+{
+    [JsonIgnore]
+    public int QuestId { get; set; }
+    [ForeignKey("QuestId")]
+    public DBQuest DBQuest { get; set; }
+    [JsonIgnore]
+    public int Stage { get; set; }
+    public string Description { get; set; }
+    public bool Talk { get; set; } = false;
+    [JsonIgnore]
+    public int? NpcId { get; set; } = null; // For talk and bring
+    public string? NpcMessage { get; set; } = null;
+    public string? NpcAnswear { get; set; } = null;
+    public bool Kill { get; set; } = false;
+    public int? MobId { get; set; } = null;
+    public bool Bring { get; set; } = false;
+    public ItemType? ItemType { get; set; } = null;
+    public int? ItemId { get; set; } = null;
+    public int? Ammount { get; set; } = null; // For kill and bring
+}
---
+++ Server/Database/Configuration/Game/Quest/DBQuestStageConfiguration.cs
@@ -0,0 +1,84 @@
+﻿using Classes.Enums.Game;
+using Classes.Models.Game.Quest;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Database.Configuration.Game.Quest;
+
+public class DBQuestStageConfiguration : IEntityTypeConfiguration<DBQuestStage>
+{
+    public void Configure(EntityTypeBuilder<DBQuestStage> builder)
+    {
+        builder.HasData(new DBQuestStage
+        {
+            QuestId = 1,
+            Stage = 1,
+            Description = "Find the healer's necklace and bring it to her.",
+            NpcId = 1,
+            NpcMessage = "I found your necklace.",
+            NpcAnswear = "Thank you very much, here is your reward.",
+            Bring = true,
+            ItemType = ItemType.ToQuest,
+            ItemId = 5,
+            Ammount = 1
+        }, new DBQuestStage
+        {
+            QuestId = 2,
+            Stage = 1,
+            Description = "Kill 30 rabbits.",
+            Kill = true,
+            MobId = 1,
+            Ammount = 30
+        }, new DBQuestStage
+        {
+            QuestId = 2,
+            Stage = 2,
+            Description = "Tell Eldrik that you have killed enough rabbits.",
+            Talk = true,
+            NpcId = 2,
+            NpcMessage = "I killed rabbits.",
+            NpcAnswear = "Thank you very much, here is your reward."
+        }, new DBQuestStage
+        {
+            QuestId = 3,
+            Stage = 1,
+            Description = "Kill 30 spiders.",
+            Kill = true,
+            MobId = 2,
+            Ammount = 30
+        }, new DBQuestStage
+        {
+            QuestId = 3,
+            Stage = 2,
+            Description = "Tell Franko that you have killed enough spiders.",
+            Talk = true,
+            NpcId = 4,
+            NpcMessage = "I killed spiders.",
+            NpcAnswear = "Thank you very much, here is your reward."
+        }, new DBQuestStage
+        {
+            QuestId = 4,
+            Stage = 1,
+            Description = "Bring 3 apples for cake to Adelaide.",
+            NpcId = 1,
+            NpcMessage = "I brought you three apples.",
+            NpcAnswear = "Thank you very much, here is your reward.",
+            Bring = true,
+            ItemType = ItemType.Consumables,
+            ItemId = 10,
+            Ammount = 3
+        }, new DBQuestStage
+        {
+            QuestId = 5,
+            Stage = 1,
+            Description = "Bring 5 night mashroom to Gustaf.",
+            NpcId = 3,
+            NpcMessage = "I brought you five night mashrooms.",
+            NpcAnswear = "Thank you very much, here is your reward.",
+            Bring = true,
+            ItemType = ItemType.Consumables,
+            ItemId = 12,
+            Ammount = 5
+        });
+    }
+}
