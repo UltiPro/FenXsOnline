8593c24efea98e42b4a4185d2aab370cb2a9eeea
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-12-30 21:16:34 +0100

    Update DBHeroQuest.cs

--- Server/Classes/Models/Game/Quest/DBHeroQuest.cs
+++ Server/Classes/Models/Game/Quest/DBHeroQuest.cs
@@ -1,5 +1,15 @@
-﻿namespace Classes.Models.Game.Quest;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Classes.Models.Game.Hero;
+
+namespace Classes.Models.Game.Quest;
 
 public class DBHeroQuest
 {
+    public int HeroId { get; set; }
+    [ForeignKey("HeroId")]
+    public DBHero DBHero { get; set; }
+    public int QuestId { get; set; }
+    [ForeignKey("QuestId")]
+    public DBQuest DBQuest { get; set; }
+    public bool Done { get; set; } = false;
 }
