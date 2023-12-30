b4c8cf24a9af0262574fa6da403a3a14de5dfba9
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-12-30 19:18:03 +0100

    Update MapData.cs

--- Server/Classes/Models/Game/Map/MapData.cs
+++ Server/Classes/Models/Game/Map/MapData.cs
@@ -1,13 +1,6 @@
-﻿using Classes.Models.Game.Npc;
+﻿namespace Classes.Models.Game.Map;
 
-namespace Classes.Models.Game.Map;
-
-public class MapData : MapDataRefresh
+public class MapData : MapDataResponse
 {
-    public string Name { get; set; }
-    public string SpriteURL { get; set; }
-    public bool IsPvP { get; set; }
     public bool[,] Fields { get; set; }
-    public List<MapDoor>? Doors { get; set; } = null;
-    public List<DBNpc>? NPCs { get; set; } = null;
 }
