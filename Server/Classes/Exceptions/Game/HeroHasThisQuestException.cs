208974b2de1e40d00a2b355cfb128d65466bcc8f
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2023-12-31 22:58:39 +0100

    Create HeroHasThisQuestException.cs

---
+++ Server/Classes/Exceptions/Game/HeroHasThisQuestException.cs
@@ -0,0 +1,6 @@
+﻿namespace Classes.Exceptions.Game;
+
+public class HeroHasThisQuestException : Exception
+{
+    public HeroHasThisQuestException() : base("The hero has completed or is in the process of completing this quest.") { }
+}
