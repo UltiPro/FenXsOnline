66fe7766ddfe6c6d46f6eb6594e7308ba429715e
Ergon4321 <85365948+Ergon4321@users.noreply.github.com>
2023-12-31 05:24:17 +0100

    Create Monster.js

---
+++ Client/js/game/Monster.js
@@ -0,0 +1,36 @@
+class Monster extends GameObject{
+    constructor(config){
+        super(config)
+        this.isDead = config.isDead || false;
+
+    }
+
+    killed(){
+        this.isDead = true;
+    }
+
+    update(state) {
+        this.updateSprite(state);
+        this.handleOpacityChange(state);
+    }
+
+    updateSprite() {
+        this.sprite.setAnimation("idle-" + this.direction);
+    }
+
+    mount() {
+        this.isMounted = true;
+    }
+
+    //chaging sprite opacity when hero is behind an object
+    handleOpacityChange(state) {
+        //reading coords with zindex field
+        const isBehindObject =
+            state.map.zindex[`${this.x},${this.y}`] || false;
+
+        if (isBehindObject !== this.isBehindObject) {
+            this.isBehindObject = isBehindObject;
+            this.sprite.isBehindObject = isBehindObject; // Update sprite opacity
+        }
+    }
+}
