12190989a3986fb246d684d2498a9ab30be22a6f
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2024-01-03 19:01:51 +0100

    Create QuestStageResponse.cs

---
+++ Server/Classes/Models/Game/Quest/QuestStageResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Classes.Models.Game.Quest;
+
+public class QuestStageResponse
+{
+    [JsonIgnore]
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+}
