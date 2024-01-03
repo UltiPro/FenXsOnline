add0081e484f04f05d8362efce5a6a565dad16f5
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2024-01-03 17:19:34 +0100

    Update QuestResponse.cs

--- Server/Classes/Models/Game/Quest/QuestResponse.cs
+++ Server/Classes/Models/Game/Quest/QuestResponse.cs
@@ -2,4 +2,8 @@
 
 public class QuestResponse
 {
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string NpcMessage { get; set; }
+    public int Level { get; set; } = 1;
 }
