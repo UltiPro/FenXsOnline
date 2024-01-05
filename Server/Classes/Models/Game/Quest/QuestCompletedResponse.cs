19ff98ad673393dc408aae1585c4ad504b2eada6
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2024-01-05 02:36:26 +0100

    Create QuestCompletedResponse.cs

---
+++ Server/Classes/Models/Game/Quest/QuestCompletedResponse.cs
@@ -0,0 +1,9 @@
+﻿using Classes.Models.Game.Hero;
+
+namespace Classes.Models.Game.Quest;
+
+public class QuestCompletedResponse
+{
+    public PromotionResponse PromotionResponse { get; set; }
+    public List<DBHeroEquipment> HeroEquipmentRewards { get; set; }
+}
