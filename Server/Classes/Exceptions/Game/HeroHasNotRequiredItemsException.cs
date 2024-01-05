df960156ef89a33a97b38edad0f56aca643854cc
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2024-01-05 02:36:43 +0100

    Create HeroHasNotRequiredItemsException.cs

---
+++ Server/Classes/Exceptions/Game/HeroHasNotRequiredItemsException.cs
@@ -0,0 +1,6 @@
+﻿namespace Classes.Exceptions.Game;
+
+public class HeroHasNotRequiredItemsException : Exception
+{
+    public HeroHasNotRequiredItemsException() : base("Hero has not the required items.") { }
+}
