795005aae911e310ce4b5093146b4a50e57b4ca9
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2024-01-05 02:36:16 +0100

    Update DBQuestReward.cs

--- Server/Classes/Models/Game/Quest/DBQuestReward.cs
+++ Server/Classes/Models/Game/Quest/DBQuestReward.cs
@@ -1,16 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
-using Classes.Enums.Game;
+using Classes.Models.Game.Item;
 
 namespace Classes.Models.Game.Quest;
 
-public class DBQuestReward
+public class DBQuestReward : ItemProvider
 {
     [JsonIgnore]
     public int QuestId { get; set; }
     [ForeignKey("QuestId")]
     public DBQuest DBQuest { get; set; }
-    public ItemType ItemType { get; set; }
-    public int ItemId { get; set; }
     public int Quantity { get; set; } = 1;
 }
