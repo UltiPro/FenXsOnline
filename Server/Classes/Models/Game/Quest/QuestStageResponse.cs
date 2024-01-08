fd4e9b9965c0db72f9aab07ca7581249f38fc9e4
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2024-01-08 20:13:51 +0100

    Update QuestStageResponse.cs

--- Server/Classes/Models/Game/Quest/QuestStageResponse.cs
+++ Server/Classes/Models/Game/Quest/QuestStageResponse.cs
@@ -1,11 +1,9 @@
-﻿using System.Text.Json.Serialization;
-
-namespace Classes.Models.Game.Quest;
+﻿namespace Classes.Models.Game.Quest;
 
 public class QuestStageResponse
 {
-    [JsonIgnore]
     public int Id { get; set; }
+    public int Level { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
 }
