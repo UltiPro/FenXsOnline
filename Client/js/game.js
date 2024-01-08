a75283741d63485e233ad680163feaee575a9c17
Dawid Majewski <ergon4321@wp.pl>
2024-01-08 22:05:30 +0100

    Update game.js

--- Client/js/game.js
+++ Client/js/game.js
@@ -1,6 +1,5 @@
 $('#leave-button').bind("click", () =>{
-    app.get(apiBaseUrl + "Hero/leave").then(
+    app.put(apiBaseUrl + "Hero/leave").then(
         window.location.replace("./start.html")
     )
-  
 });
