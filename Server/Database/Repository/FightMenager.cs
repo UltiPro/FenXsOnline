0a36359b701629a2ae921589ba9d5a07dbee3aff
Patryk Wójtowicz <106282113+UltiPro@users.noreply.github.com>
2024-01-09 02:28:53 +0100

    Update FightMenager.cs

--- Server/Database/Repository/FightMenager.cs
+++ Server/Database/Repository/FightMenager.cs
@@ -140,7 +140,7 @@ public class FightMenager : IFightMenager
 
         return new FightResponse
         {
-            Promotion = playerWin ? await _promotionMenager.Promotion(hero, mob.Level, false) : null,
+            Promotion = playerWin ? _promotionMenager.Promotion(hero, mob.Level, false) : null,
             Drop = playerWin ? await DropForPlayer(hero, mob) : null,
             Dead = playerWin ? null : await DeadPlayer(hero),
             HealthPoints = hero.HealthPoints,
